ebb7c51b83ba549df25eaab99135eec4e884d979
Adrian Sieber <mail@adriansieber.com>
2016-04-18 08:37:53 +0000

    Outsource module from main feram repo

---
+++ index.js
@@ -0,0 +1,31 @@
+import fixTyposInFile from './fixTyposInFile'
+
+export default (options = {}) => {
+	const {fileTree} = options
+
+	return new Promise((resolve, reject) => {
+		let fileEditPromiseChain = Promise.resolve()
+		let commitWasCreated = false
+
+		const walker = fileTree.walk(true)
+		walker.on('error', error => reject(error))
+		walker.on('entry', (entry) => {
+			fileEditPromiseChain = fileEditPromiseChain
+				.then(commitId => {
+					if (commitId) {
+						commitWasCreated = true
+					}
+					return fixTyposInFile(
+						Object.assign({}, {entry}, options)
+					)
+				})
+		})
+		walker.on('end', () => {
+			resolve(fileEditPromiseChain
+				.then(() => console.log())
+				.then(() => commitWasCreated)
+			)
+		})
+		walker.start()
+	})
+}
